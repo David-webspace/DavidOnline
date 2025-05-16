74aaced6444824fac46290c8f319aed1b2943fd0
David-webspace <web.affabledavid5288@gmail.com>
2025-05-17 00:30:59 +0800

    Update Image 1.4

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         hostname: 'randomuser.me',
       },
     ],
-    domains: ['images.unsplash.com', 'images.pexels.com'],
+    domains: ['images.unsplash.com', 'images.pexels.com', 'somecdn.com'],
   },
 };
 
