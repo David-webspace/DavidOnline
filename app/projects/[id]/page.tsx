ecba0e994f622df0f138752cba5999929407526b
David-webspace <web.affabledavid5288@gmail.com>
2025-05-16 23:32:49 +0800

    Version Update 1.3

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,17 +1,16 @@
 import { notFound } from "next/navigation";
-import dynamic from "next/dynamic";
 
 interface ProjectPageProps {
   params: { id: string };
 }
 
 const page = async ({ params }: ProjectPageProps) => {
-  // Slug should match the filename in components/projects (e.g., mun-society-website)
+  // Slug should match the filename in components/projects (e.g., mst)
   const componentPath = `../../components/projects/${params.id}`;
   let ProjectComponent;
   try {
     ProjectComponent = (await import(componentPath)).default;
-  } catch (error) {
+  } catch {
     notFound();
   }
   return <ProjectComponent />;
