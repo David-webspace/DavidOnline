d324c6557134b893d0c510d6ea05a97ecd4ce460
David-webspace <web.affabledavid5288@gmail.com>
2025-05-17 00:18:29 +0800

    Version Update 1.3

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from "next/navigation";
-
 export default async function Page({ params }: { params: { id: string } }) {
   // Slug should match the filename in components/projects (e.g., mst)
   const componentPath = `../../components/projects/${params.id}`;
