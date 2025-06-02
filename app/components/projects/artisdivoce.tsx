fbc670cc66fec8c3068f6eb12f5ec2a02933965d
David-webspace <web.affabledavid5288@gmail.com>
2025-06-02 20:02:42 +0800

    Project Update

--- app/components/projects/artisdivoce.tsx
+++ app/components/projects/artisdivoce.tsx
@@ -4,10 +4,10 @@ import ProjectDetail from "../ProjectDetail";
 export default function ArtisdivoceProject() {
   return (
     <ProjectDetail
-      title="娜拉坡 Artisdivoce"
+      title="娜拉坡香港 Artisdivoce HK"
       banner="/artisdivoce_banner.webp"
       category="Website"
-      url="https://www.artisdivocehk.myshopify.com/"
+      url="https://artis-di-voce-hk.myshopify.com/"
       about={[
         "Artisdivoce是香港本地的電商平台，專門銷售化妝品、護膚品和美容產品。平台設計旨在為客戶提供流暢的購物體驗，並提供易於使用的界面、安全的支付處理和簡單的產品搜索功能。平台使用Shopify，一個流行的電商平台，並針對移動設備進行優化，確保客戶可以在路上購物。",
         "Artisdivoce是香港本地的電商平台，專門銷售化妝品、護膚品和美容產品。平台設計旨在為客戶提供流暢的購物體驗，並提供易於使用的界面、安全的支付處理和簡單的產品搜索功能。平台使用Shopify，一個流行的電商平台，並針對移動設備進行優化，確保客戶可以在路上購物。"
